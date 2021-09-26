50fda4b0711674bf805b81e4c227db197c289cba
kimjunsoo <rlawnstn01023@naver.com>
2021-09-26 14:12:38 +0900

    Initial Commit

---
+++ Delegate_train/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Delegate_train
+//
+//  Created by 준수김 on 2021/09/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
