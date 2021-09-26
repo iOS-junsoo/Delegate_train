49b6eeb75716701de449c73706e4cb973fb0af97
kimjunsoo <rlawnstn01023@naver.com>
2021-09-26 14:35:22 +0900

    Zedd님의 블로그 실습

--- Delegate_train/ViewController.swift
+++ Delegate_train/ViewController.swift
@@ -1,19 +1,44 @@
+// [Delegate x]
+
+//import UIKit
 //
-//  ViewController.swift
-//  Delegate_train
+//class ViewController: UIViewController {
 //
-//  Created by 준수김 on 2021/09/26.
+//    @IBOutlet weak var enterLabel: UILabel!
+//    @IBOutlet weak var textField: UITextField!
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        // Do any additional setup after loading the view.
+//    }
+//    @IBAction func buttonClicked(_ sender: UIButton) {
+//        enterLabel.text = textField.text;
+//    }
 //
+//
+//}
+
+// [Delegate o]
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    //UITextFieldDelegate '채택'
 
+    @IBOutlet weak var enterLabel: UILabel!
+    @IBOutlet weak var textField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        textField.delegate = self
+        //위임자(대리자)가 누구인지 알려주는 과정
+        //"너(textField)한테 이벤트가 발생하면 프로토콜에 따라 내(ViewController)가 너(textField)에게 응답을 줄게."
+        
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+            enterLabel.text = textField.text
+            return true
     }
 
+    
 
 }
 
